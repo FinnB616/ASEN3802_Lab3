e96e82db0a5ed6bbc12eae7329f4989b86b80766
IanvandeLindt <iava4226@colorado.edu>
2026-03-27 15:36:10 -0600

    fixed slope issue

--- NACA_gen.m
+++ NACA_gen.m
@@ -4,28 +4,31 @@ close all;
 
 %[x_b,y_b] = function NACA_Airfoils(m,p,t,c,N)
 
-m=.04;
+m=.2;
 p=.4;
 t=.12;
 c=1;
-N=1000;
+N=10000;
 
-%create n/2 x positions
+%create n/2 different x positions
 x_positions = linspace(0,c,N/2);
 
+%find the thickness at each x position
 y_t = ((t./0.2).*c).* ((.2969.*sqrt(x_positions./c)) - (.1260.*(x_positions./c)) - (.3516.*((x_positions./c).^2)) + (.2843.*((x_positions./c).^3)) - (.1036.*((x_positions./c).^4)));
 
-
+%split the positions into before the max thickness and after
 front_positions = x_positions(x_positions < (p*c));
 rear_positions = x_positions(x_positions >= (p*c));
 
+%create the mean camber line (different eqautions infront and behind the
+%max thickness)
 y_c = (m.*front_positions./(p.^2)).*((2.*p) - (front_positions./c));
 y_c = [y_c (m.*(c-rear_positions)./((1-p).^2)).*(1+(rear_positions./c)-(2.*p))];
 
-for i=1:(length(x_positions)-1)
-    zeta(1)=0;
-    zeta(i+1) = atan((y_c(i+1)-y_c(i))/(x_positions(i+1)-x_positions(i)));
-end
+dyc_dx = (2*m/p) - (2.*m.*front_positions./(p^2 * c));
+dyc_dx = [dyc_dx (((2*m)/((1-p).^2)).*(p-(rear_positions./c)))];
+zeta = atan(dyc_dx);
+
 x_U = x_positions - (y_t.*sin(zeta));
 y_U = y_c + y_t.*cos(zeta);
 x_L = x_positions + (y_t.*sin(zeta));
