e2b0754b5e8d6047abd71fe5d471c4d5e10295c1
FinnB616 <Finn.Boland@colorado.edu>
2026-04-03 15:20:12 -0600

    Update main.m

--- main.m
+++ main.m
@@ -45,5 +45,75 @@ legend('0021 Surface','0021 Panel Points', ...
 
 %% Task 2: Convergence Study
 
+%% Task 3: Effect of Airfoil Thickness on Lift
+
+%given minimum panels needed in task 2 = 70  
+
+% random freestream velo
+VINF = 100; %m/s?
+
+% aoa alpha ranges from -5 to 15 degrees
+alpha = -5:1:15;
+
+% NACA 0006
+[x31, y31, xc31, yc31] = NACA_Airfoil(0.0, 0.0, 0.06, c, N_min);
+
+% NACA 0012
+[x32, y32, xc32, yc32] = NACA_Airfoil(0.0, 0.0, 0.12, c, N_min);
+
+% NACA 0018
+[x33, y33, xc33, yc33] = NACA_Airfoil(0.0, 0.0, 0.18, c, N_min);
+
+% lift coeff arrays
+cl_06 = zeros(size(alpha));
+cl_12 = zeros(size(alpha));
+cl_18 = zeros(size(alpha));
+
+% call VPM for each AF at each aoa
+for i = 1:length(alpha)
+    cl_06(i)=Vortex_Panel(x31,y31,VINF,alpha(i));
+    cl_12(i)=Vortex_Panel(x32,y32,VINF,alpha(i));
+    cl_18(i)=Vortex_Panel(x33,y33,VINF,alpha(i));
+end
+    
+
+% symmetric AF -> thin airfoil theory
+cl_TAT = 2*pi*deg2rad(alpha);
+
+% use fit to find lift slope and L=0 aoa
+idx = (alpha >= -4) & (alpha <= 8);
+
+p06 = polyfit(alpha(idx), cl_06(idx), 1);
+p12 = polyfit(alpha(idx), cl_12(idx), 1);
+p18 = polyfit(alpha(idx), cl_18(idx), 1);
+
+a0_06 = p06(1);
+a0_12 = p12(1);
+a0_18 = p18(1);
+
+alphaL0_06 = -p06(2)/p06(1);
+alphaL0_12 = -p12(2)/p12(1);
+alphaL0_18 = -p18(2)/p18(1);
+
+a0_TAT = 2*pi*(pi/180);
+alphaL0_TAT = 0;
+
+%Plotting
+figure
+hold on
+grid on
+box on
+
+
+plot(alpha, cl_06, 'b-', 'LineWidth', 1.5, 'MarkerSize', 5, 'DisplayName', 'VPM NACA 0006')
+plot(alpha, cl_12, 'r-', 'LineWidth', 1.5, 'MarkerSize', 5, 'DisplayName', 'VPM NACA 0012')
+plot(alpha, cl_18, 'g-', 'LineWidth', 1.5, 'MarkerSize', 5, 'DisplayName', 'VPM NACA 0018')
+plot(alpha, cl_TAT, 'k--', 'LineWidth', 2, 'DisplayName', 'Thin Airfoil Theory')
+
+xlabel('\alpha (deg)')
+ylabel('c_l')
+title('Effect of Airfoil Thickness on Lift')
+legend('Location','best')
+
 
 
