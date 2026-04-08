e8ee54b5baea3db4cc3f8ff992b2160923c39fa6
Emerson Cuffe <emcu6096@colorado.edu>
2026-04-08 13:56:30 -0600

    Update main.m

--- main.m
+++ main.m
@@ -48,6 +48,7 @@ legend('0021 Surface','0021 Panel Points', ...
 %% Task 3: Effect of Airfoil Thickness on Lift
 
 %given minimum panels needed in task 2 = 70  
+N_min = 70;
 
 % random freestream velo
 VINF = 100; %m/s?
@@ -75,7 +76,6 @@ for i = 1:length(alpha)
     cl_12(i)=Vortex_Panel(x32,y32,VINF,alpha(i));
     cl_18(i)=Vortex_Panel(x33,y33,VINF,alpha(i));
 end
-    
 
 % symmetric AF -> thin airfoil theory
 cl_TAT = 2*pi*deg2rad(alpha);
@@ -115,5 +115,82 @@ ylabel('c_l')
 title('Effect of Airfoil Thickness on Lift')
 legend('Location','best')
 
+%% Task 4: Effect of Airfoil Camber on Lift
+
+% Function inputs
+VINF = 100;
+ALPHA = -5:15;
 
 
+% From Task 2: optimal number of panels is 70
+N = 70;
+
+% NACA 0012
+m1 = 0;
+p1 = 0;
+t1 = 0.12;
+[x1, y1, xc1, yc1] = NACA_Airfoil(m1, p1, t1, c, N);
+
+% NACA 2412 (Moderately Cambered Airfoil)
+
+m1 = 0.02;
+p1 = 0.4;
+t1 = 0.12;
+[x2, y2, xc2, yc2] = NACA_Airfoil(m2, p2, t2, c, N);
+
+% NACA 4412 (Significantly Cambered Airfoil)
+
+m3 = 0.04;
+p3 = 0.4;
+t3 = 0.12;
+[x3, y3, xc3, yc3] = NACA_Airfoil(m3, p3, t3, c, N);
+
+
+% Vortex Panel Function for varying alpha
+
+for i = 1:length(ALPHA)
+    CL1(i) = Vortex_Panel(x1, y1, VINF, ALPHA(i)); % Symmetric
+    CL2(i) = Vortex_Panel(x2, y2, VINF, ALPHA(i)); % Moderately Cambered
+    CL3(i) = Vortex_Panel(x3, y3, VINF, ALPHA(i)); % Significantly Cambered
+end
+
+% interpolating alpha to find exactly when CL is predicted to be zero
+
+alpha0_0012 = interp1(CL1, ALPHA, 0);
+alpha0_2412 = interp1(CL2, ALPHA, 0);
+alpha0_4412 = interp1(CL3, ALPHA, 0);
+
+% Thin Airfoil Theory, assuming x-inter to be 0, -2, -4 respectfully
+
+% TAT uses radians
+alpha_rad = deg2rad(ALPHA);
+
+% CL / alpha is always 2pi with different x intercepts 
+
+CL_TAT_0012 = 2*pi*(alpha_rad - deg2rad(0));
+CL_TAT_2412 = 2*pi*(alpha_rad - deg2rad(-2));
+CL_TAT_4412 = 2*pi*(alpha_rad - deg2rad(-4));
+
+% Use poly fit function to find slope of each airfoil 
+slope_0012 = polyfit(ALPHA, CL1, 1);
+slope_2412 = polyfit(ALPHA, CL2, 1);
+slope_4412 = polyfit(ALPHA, CL3, 1);
+figure
+
+% Vortex Panel
+plot(ALPHA, CL1, 'b-', 'LineWidth', 1)
+hold on
+plot(ALPHA, CL2, 'r-', 'LineWidth', 1)
+plot(ALPHA, CL3, 'g-', 'LineWidth', 1)
+
+% Thin Airfoil Theory 
+plot(ALPHA, CL_TAT_0012, 'b--')
+plot(ALPHA, CL_TAT_2412, 'r--')
+plot(ALPHA, CL_TAT_4412, 'g--')
+
+xlabel('\alpha (deg)')
+ylabel('Cl')
+title('Effect of Airfoil Camber on Lift')
+legend('NACA 0012','NACA 2412', 'NACA 4412', 'Thin Airfoil 0012', 'Thin Airfoil 2412', 'Thin Airfoil 4412' )
+grid on
+hold off
